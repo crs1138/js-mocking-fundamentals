9479df495eff08f97c80f27045f50e11d7ee90ae
Honza <info@planxdesign.eu>
2023-06-29 18:10:25 +0000

    Monkey patch of utils.getWinner()

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,5 +1,12 @@
 const assert = require("assert");
 const thumbWar = require("../thumb-war");
+const utils = require("../utils");
+
+const originalGetWinner = utils.getWinner;
+utils.getWinner = (p1, p2) => p1;
 
 const winner = thumbWar("Alex", "Tato");
 assert.strictEqual(winner, "Alex");
+
+// clean up
+utils.getWinner = originalGetWinner;
