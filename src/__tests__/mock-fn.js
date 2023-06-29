10baea6f219b1ca995e7d361d529998c621b3ae8
Jan Honza Pozivil <jpozivil@leadtech.com>
2023-06-29 22:12:26 +0200

    call functions correctly - finished

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -8,6 +8,15 @@ test('returns winner', () => {
     const winner = thumbWar('Johnny', 'Joan');
     expect(winner).toBe('Johnny');
 
+    // expect(utils.getWinner).toHaveBeenCalledTimes(2);
+    // expect(utils.getWinner).toHaveBeenCalledWith('Johnny', 'Joan');
+    // expect(utils.getWinner).toHaveBeenNthCalledWith(1, 'Johnny', 'Joan');
+    // expect(utils.getWinner).toHaveBeenNthCalledWith(2, 'Johnny', 'Joan');
+    // console.log(utils.getWinner.mock.calls);
+    expect(utils.getWinner.mock.calls).toEqual([
+        ['Johnny', 'Joan'],
+        ['Johnny', 'Joan'],
+    ]);
     // clean up
     utils.getWinner = originalGetWinner;
 });
--- src/no-framework/mock-fn.js
+++ src/no-framework/mock-fn.js
@@ -4,13 +4,24 @@ const utils = require('../utils');
 
 // create a mock function that takes an implementation as an argument
 // and stores its arguments it has been called with in an array `calls`
-function fn(impl) {}
+function fn(impl) {
+    const mockFn = (...args) => {
+        mockFn.mock.calls.push(args);
+        return impl(...args);
+    };
+    mockFn.mock = { calls: [] };
+    return mockFn;
+}
 
 const originalGetWinner = utils.getWinner;
 utils.getWinner = fn((p1, p2) => p1);
 
 const winner = thumbWar('Alex', 'Tato');
 assert.strictEqual(winner, 'Alex');
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+    ['Alex', 'Tato'],
+    ['Alex', 'Tato'],
+]);
 
 // clean up
 utils.getWinner = originalGetWinner;
